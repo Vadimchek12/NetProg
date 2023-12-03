bcd50a17dd94365dcb7b2998aed1a2f4238064f4
VadimShcheglov <145258704+Vadimchek12@users.noreply.github.com>
2023-12-04 00:16:51 +0300

    Create main.cpp

--- daytime/main.cpp
+++ daytime/main.cpp
@@ -1,70 +1,40 @@
-#include <netinet/in.h>
 #include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
-#include <string>
-#include <ctime>
-using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-    struct sockaddr_in {
-        short sin_family;
-        unsigned short sin_port;
-        struct in_addr sin_addr;
-        char sin_zero[8];
-    };
-    struct in_addr {
-        unsigned long s_addr;
-    };
+    int sockfd;
+    char buffer[4096];
+    struct sockaddr_in server_addr;
 
-    int s = socket(AF_INET, SOCK_DGRAM, 0);
-    if (s == -1) {
-        cout << "Socket error\n";
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        std::cerr << "Error creating socket" << std::endl;
         return 1;
     }
 
-    sockaddr_in * self_addr = new (sockaddr_in);
-    self_addr->sin_family = AF_INET;
-    self_addr->sin_port = htons(12092);
-    self_addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(13);  // порт службы daytime
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    int b = bind(s,(const sockaddr*) self_addr,sizeof(sockaddr_in));
-    if(b == -1) {
-        cout << "Binding error\n";
-        close(s);
+    if (sendto(sockfd, " ", 1, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Error sending data" << std::endl;
         return 1;
     }
-    while(true) {
-        char connect[50];
-        time_t now = time(0);
-        tm *ltm = localtime(&now);
-        string day, monday, hour, minute, second;
-        string daytime;
-        ltm->tm_mday < 10 ? day = '0' + to_string(ltm->tm_mday) : day = to_string(ltm->tm_mday);
-        ltm->tm_mon < 10 ? monday = '0' + to_string(ltm->tm_mon + 1) : monday = to_string(ltm->tm_mon + 1);
-        ltm->tm_hour < 10 ? hour = '0' + to_string(ltm->tm_hour) : hour = to_string(ltm->tm_hour);
-        ltm->tm_min < 10 ? minute = '0' + to_string(ltm->tm_min) : minute = to_string(ltm->tm_min);
-        ltm->tm_sec < 10 ? second = '0' + to_string(ltm->tm_sec) : second = to_string(ltm->tm_sec);
-        daytime = day + '.' + monday + '.' + to_string(ltm->tm_year + 1900) + ' ' + hour + ':' + minute + ':' + second;
-        char daytime_msg[daytime.size() + 1];
-        strcpy(daytime_msg, daytime.c_str());
-        sockaddr_in * client_addr = new sockaddr_in;
-        socklen_t len = sizeof (sockaddr_in);
-        int bytes_received = recvfrom(s, connect, sizeof(connect), 0, (sockaddr*)(client_addr), &len);
-        if(bytes_received < 0) {
-            cout << "Error receiving from client\n";
-        } else {
-            cout << "Successful client connection!\n";
-            int bytes_sent = sendto(s, daytime_msg, strlen(daytime_msg), 0, (sockaddr*)(client_addr), len);
-            if(bytes_sent < 0) {
-                cout << "Error sending daytime to client\n";
-            } else {
-                cout << "Daytime was sent to client!\n";
-            }
-        }
+
+    int n;
+    socklen_t len;
+    if ((n = recvfrom(sockfd, (char *)buffer, 4096, 0, (struct sockaddr *)&server_addr, &len)) < 0) {
+        std::cerr << "Error receiving data" << std::endl;
+        return 1;
     }
-    close(s);
+
+    buffer[n] = '\0';
+    std::cout << "Ответ от сервера: " << buffer << std::endl;
+
+    close(sockfd);
     return 0;
 }
