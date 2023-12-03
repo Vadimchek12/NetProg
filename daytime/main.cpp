29a5f72c217b1a8f03272b92dbbdfa90596a0143
VadimShcheglov <145258704+Vadimchek12@users.noreply.github.com>
2023-12-04 00:21:06 +0300

    Create main.cpp

--- daytime/main.cpp
+++ daytime/main.cpp
@@ -17,7 +17,7 @@ int main()
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(13);  // порт службы daytime
+    server_addr.sin_port = htons(12345);  // порт службы daytime
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (sendto(sockfd, " ", 1, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
